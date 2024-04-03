3ca4d44c6dc922c7efce94bbd4da996c02c6ccf8
Channy <117459099+channyhow@users.noreply.github.com>
2024-04-03 22:24:31 +0200

    new version

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,13 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 // global.d.ts
 
-// Define a general type for dataLayer objects
-interface DataLayerObject {
-  event?: string;
-  [key: string]: any; // Allow for other dynamic keys with unknown types
-}
-
-// Extend the Window interface to include dataLayer with the specific type
 interface Window {
-  dataLayer: DataLayerObject[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  dataLayer: any[];
 }
